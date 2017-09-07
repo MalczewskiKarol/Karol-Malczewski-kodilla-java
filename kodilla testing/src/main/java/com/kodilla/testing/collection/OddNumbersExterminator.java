96d8336bc9e4ed6c11805a2f321bacda719dedc3
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-09-07 17:40:28 +0200

    6.3

---
+++ kodilla testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,31 @@
+package com.kodilla.testing.collection;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+public class OddNumbersExterminator {
+
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+        return oddNumbers;
+    }
+
+
+    public static void main (String[] args) {
+
+        public List<Integer> numbers() {
+
+        ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+        oddNumbers.add(1);
+        oddNumbers.add(3);
+        oddNumbers.add(5);
+        oddNumbers.add(7);
+        oddNumbers.add(9);
+
+        return(oddNumbers);
+    }
+};
--- kodilla testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -2,15 +2,20 @@ package com.kodilla.testing.user;
 
 public class SimpleUser {
 
-    private String username;
+    String username;
+    String realName;
 
-    public SimpleUser(String username) {
 
+    public SimpleUser(String username, String realName) {
         this.username = username;
+        this.realName = realName;
     }
 
     public String getUserName() {
-
         return  username;
+    }
+
+    public String getRealName() {
+        return realName;
     }
 }
