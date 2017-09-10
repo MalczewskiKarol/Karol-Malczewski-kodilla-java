64ae9f37d724f5fe37e30ff145cf342829c9c51a
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-09-10 14:56:59 +0200

    6.3

--- kodilla testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -6,21 +6,11 @@ import java.util.Random;
 
 public class OddNumbersExterminator {
 
-    public static void exterminate(ArrayList<Integer> numbers) {
-        int num = 0;
-        for (int i = 0; i < numbers.size(); i++) {
-            num = numbers.get(i);
-            if (num % 2 == 0) {
-                ArrayList<Integer> oddNumbers = null;
-                oddNumbers.add(num);
-            }
-        }
-    }
-
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+
 
         Random theGenerator = new Random();
         for (int i = 0; i < 20; i++) {
@@ -29,6 +19,16 @@ public class OddNumbersExterminator {
 
         System.out.println(numbers);
         System.out.println("=============================");
-        System.out.println(oddNumbers);
+        System.out.println(evenNumbers);
+
+        public void exterminate(ArrayList<Integer> numbers) {
+            int num = 0;
+            for (int i = 0; i < numbers.size(); i++) {
+                num = numbers.get(i);
+                if (num % 2 == 0) {
+                    evenNumbers.add(num);
+                }
+            }
+        }
     }
 }
