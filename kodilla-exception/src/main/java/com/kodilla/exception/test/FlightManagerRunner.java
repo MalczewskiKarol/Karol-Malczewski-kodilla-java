c6b7a6ee9b8dbdbc455ffb24dfb388529f5532ee
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-09-26 15:51:03 +0200

    8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightManagerRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightManagerRunner.java
@@ -1,10 +1,5 @@
 package com.kodilla.exception.test;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-
 public class FlightManagerRunner {
 
     public static void main(String[] args) throws Exception {
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,7 +1,5 @@
 package com.kodilla.exception.test;
 
-import java.util.HashMap;
-
 public class RouteNotFoundException extends Exception {
 
     public RouteNotFoundException(final String findFlight) {
