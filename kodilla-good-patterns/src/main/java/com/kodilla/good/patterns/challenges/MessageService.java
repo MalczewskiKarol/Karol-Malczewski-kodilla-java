024ed49fad1672902f94dbdbd52ba72d40efe87c
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-09-28 20:33:08 +0200

    9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MessageService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MessageService.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.challenges;
 public class MessageService implements InformationService {
 
     public void inform(User user) {
-        MessageService messageService = new MessageService();
-        messageService.inform(user);
+        System.out.println("Order has been successfully placed.");
+
     }
 }
