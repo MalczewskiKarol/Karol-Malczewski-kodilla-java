fe9ec9c118ea671ec38f56fd3488ee81909731e0
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-11-28 14:14:18 +0100

    7.3.1

--- kodilla-stream-v2/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream-v2/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,10 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
@@ -15,11 +17,15 @@ public class StreamMain {
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
+        System.out.println("=============================");
+
         System.out.println("CALCULATING EXPRESSIONS WITH METHOD REFERENCES");
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("=============================");
 
         System.out.println("BEUTIFULIER");
 
@@ -27,9 +33,27 @@ public class StreamMain {
         poemBeautifier.beautify("AAA", "BBB", "CCC", (a, b, c) -> a + "Text" + a);
         poemBeautifier.beautify("AAA", "BBB", "CCC", (a, b, c) -> b + "Text" + b);
         poemBeautifier.beautify("AAA", "BBB", "CCC", (a, b, c) -> c + "Text" + c);
-        poemBeautifier.beautify("AAA", "BBB", "CCC", (a, b, c) -> b + "Text".toUpperCase() + b);
+        poemBeautifier.beautify("AAA", "BBB", "CCC", (a, b, c) -> "Text".toUpperCase());
+
+        System.out.println("=============================");
 
         System.out.println("Using stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+        System.out.println("=============================");
+
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(s -> System.out.println(s));
+
+        System.out.println("=============================");
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
     }
 }
