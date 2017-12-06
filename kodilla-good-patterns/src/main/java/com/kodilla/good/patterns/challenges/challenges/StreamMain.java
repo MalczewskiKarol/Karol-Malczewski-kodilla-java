48148898a35e3875161548e83fcb0c5e62760a49
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-12-06 01:11:42 +0100

    9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/challenges/StreamMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/challenges/StreamMain.java
@@ -1,12 +1,15 @@
-//package com.kodilla.good.patterns.challenges.challenges;
-//
-//public class StreamMain {
-//
-//    public static void main(String[] args) {
-//
-//        MovieStore movieStore = new MovieStore();
-//        movieStore.getMovies().entrySet().stream()
-//                .flatMap(t -> t.getValue().stream()
-//                        .map(m -> m.toString())
-//    }
-//}
+package com.kodilla.good.patterns.challenges.challenges;
+
+import java.util.stream.Collectors;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        String movies = movieStore.getMovies().entrySet().stream()
+                .flatMap(t -> t.getValue().stream())
+                        .collect(Collectors.joining(", "));
+        System.out.println(movies);
+    }
+}
