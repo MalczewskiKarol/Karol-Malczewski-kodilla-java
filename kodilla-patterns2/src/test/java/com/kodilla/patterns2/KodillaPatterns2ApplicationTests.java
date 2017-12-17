843158532a0f04127148cbf313ca877bb2243d27
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-12-17 14:10:03 +0100

    20.1

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/KodillaPatterns2ApplicationTests.java
@@ -0,0 +1,31 @@
+package com.kodilla.patterns2;
+
+import com.kodilla.patterns2.facade.api.ItemDto;
+import com.kodilla.patterns2.facade.api.OrderDto;
+import com.kodilla.patterns2.facade.api.OrderFacade;
+import com.kodilla.patterns2.facade.api.OrderProcessingException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaPatterns2ApplicationTests {
+
+	OrderFacade orderFacade;
+
+	@Test
+	public void testShopFacade() {
+		OrderDto order = new OrderDto();
+		order.addItem(new ItemDto(10L, 2));
+		order.addItem(new ItemDto(216L, 1));
+		order.addItem(new ItemDto(25L, 1));
+		order.addItem(new ItemDto(11L, 3));
+		try {
+			orderFacade.processOrder(order, 1L);
+		} catch (OrderProcessingException e) {
+			//business exception - should be handled in real application
+		}
+	}
+}
