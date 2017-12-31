0abb3b823f9e05f6a12b7a5a6752169b53da88ff
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-12-31 15:35:22 +0100

    20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
@@ -0,0 +1,41 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logProcessOrder(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("LoggingAspect.logProcessOrder(): " + proceedingJoinPoint.getSignature().getName() + ": Before Method Execution");
+        try {
+            proceedingJoinPoint.proceed();
+        } finally {
+            LOGGER.info("Logging the event");
+        }
+        System.out.println("LoggingAspect.logProcessOrder(): " + proceedingJoinPoint.getSignature().getName() + ": After Method Execution");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+}
